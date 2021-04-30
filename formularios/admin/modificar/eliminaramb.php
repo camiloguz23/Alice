bcda115ec8f964985db9a445e3510195d3705c83
jeyson <jcguzman8548@misena.edu.co>
2021-04-29 23:45:11 -0500

    archivos de eliminar de usuario y ambiente

---
+++ formularios/admin/modificar/eliminaramb.php
@@ -0,0 +1,13 @@
+<?php
+require_once ("../../../php/conexion.php");
+
+$dato = $_POST["datoeli"];
+
+$eliminarAmb = "DELETE FROM ambiente WHERE id_amb = $dato";
+$consultaAmb = mysqli_query($bdmysqli,$eliminarAmb);
+
+if ($consultaAmb){
+    header("location: modificarAmbi.php");
+}
+
+?>

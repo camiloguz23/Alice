f16029fc80b62f6db5f7bc8a258533f023e93a43
jeyson <jcguzman8548@misena.edu.co>
2021-05-27 20:05:47 -0500

    validacion del formalario de transversal

--- javascript/transversal.js
+++ javascript/transversal.js
@@ -1,12 +1,18 @@
 const boton = document.getElementById("btn_trans")
 const formu = document.getElementById("transversal")
 
+let a = 0
+let b = 0
+let c = 0
+let d = 0
+let m = 0
+
 boton.disabled= true
 boton.style.backgroundColor = "hsla(0,100%,50%,0.5)"
 
 validar()
 
-function validar(params) {
+function validar() {
     const docu = document.getElementById("docu")
     const ficha = document.getElementById("ficha")
     const materia = document.getElementById("materia")
@@ -15,14 +21,23 @@ function validar(params) {
 
     let confi = 0
     
+    
     docu.addEventListener("blur", () => {
 
         let dato = docu.value
         
         if (dato != "") {
+
+            if (confi < 0 || confi > 4) {
+                confi = 0
+            }
             
-            confi = confi + 1
-            active(confi)
+            if (a == 0) {
+                confi = confi + 1
+                a = 1
+                active(confi)
+            }
+            
 
         }else{
             alert("Seleccione un instructor")
@@ -39,8 +54,15 @@ function validar(params) {
 
         if (dato2 != "") {
             
-            confi = confi + 1
-            active(confi)
+            if (confi < 0 || confi > 4) {
+                confi = 0
+            }
+            
+            if (b == 0) {
+                confi = confi + 1
+                b = 1
+                active(confi)
+            }
 
         }else {
             alert("Seleccione un ficha de formacion")
@@ -57,8 +79,15 @@ function validar(params) {
 
         if (dato3 != "") {
 
-            confi = confi + 1
-            active(confi)
+            if (confi < 0 || confi > 4) {
+                confi = 0
+            }
+
+            if (c == 0) {
+                confi = confi + 1
+                c = 1
+                active(confi)
+            }
 
         }else {
             alert("Seleccione una materia")
@@ -75,8 +104,15 @@ function validar(params) {
 
         if (dato4 != "") {
 
-            confi = confi +1
-            active(confi)
+            if (confi < 0 || confi > 4) {
+                confi = 0
+            }
+
+            if (d == 0) {
+                confi = confi + 1
+                d = 1
+                active(confi)
+            }
             
         }else{
             alert("Seleccione una hora o fecha")
@@ -100,8 +136,11 @@ function validar(params) {
 
         if ( ejem != "" & ejem2 != "" & ejem3 != "" & ejem4 != "") {
             
-            confi = confi + 1
-            active(confi)
+            if (m == 0) {
+                confi = confi + 1
+                m = 1
+                active(confi)
+            }
         }
     })
     
@@ -109,10 +148,8 @@ function validar(params) {
 
 function active(numero) {
 
-   
-    let num = numero
-   
-    if (num == 5) {
+   console.log(numero)
+    if (numero == 5) {
         boton.disabled= false
         boton.style.backgroundColor = "rgb(89, 181, 72)"
     }
@@ -120,4 +157,52 @@ function active(numero) {
     
     
 }
+
+boton.addEventListener("click", (e) =>{
+    e.preventDefault()
+
+    const formulario = new FormData (formu)
+
+    fetch("../../../../php/trans.php", {
+        method:"POST",
+        body:formulario
+    }).then(res => res.text()).then(info => {
+
+        if(info == 1) {
+            alert("La transversal ya esta asignada a la formacion")
+            formu.reset() 
+            boton.disabled= true
+            boton.style.backgroundColor = "hsla(0,100%,50%,0.5)"
+            a = 0
+            b = 0
+            c = 0
+            d = 0
+            m = 0
+
+
+        }else if(info == 2) {
+
+            alert("No se asigno correctamente la transversal a la formacion")
+            formu.reset()
+            boton.disabled= true
+            boton.style.backgroundColor = "hsla(0,100%,50%,0.5)"
+            a = 0
+            b = 0
+            c = 0
+            d = 0
+            m = 0
+            
+        } else {
+            alert(info)
+            formu.reset()
+            boton.disabled= true
+            boton.style.backgroundColor = "hsla(0,100%,50%,0.5)"
+            a = 0
+            b = 0
+            c = 0
+            d = 0
+            m = 0
+        }
+    })
+})
 

ae170f78acba53fc824d27a5ae5c56b67d1b19ed
jeyson <jcguzman8548@misena.edu.co>
2021-06-18 20:18:21 -0500

    subida

--- php/editar.php
+++ php/editar.php
@@ -9,25 +9,24 @@ require_once("conexion.php");
 
     $foto = $_FILES['foto'] ['name'];
     
-   echo $foto;
+  
 
     if ($foto == "" || $foto == null) {
-        echo "hola1";
+        
         $actualizar = "UPDATE usuario SET celular=$celular,direccion='$direccion', email='$email',contra_seguridad = '$clave' WHERE docu= $id";
 
         $resultado = mysqli_query($bdmysqli, $actualizar);
-        echo "sds";
-
+        
         if ($resultado) {
             echo "hola2";
             header("location: ../usuarios/instructores/perfilIns.php");
         } else{
-            echo "hola3";
+         
             echo 'no se pudo actualizar';
         }
     }else {
 
-        echo "hola4";
+        
 
         $ruta = $_FILES['foto'] ['tmp_name'];
         $destino = "../usuarios/foto/".$foto;

c74dd19df3b46210a42a4181da50955b8858d4ec
jeyson <jcguzman8548@misena.edu.co>
2021-06-30 11:54:16 -0500

    arreglos de formulario de transversal y tabla de las transversales

--- php/trans.php
+++ php/trans.php
@@ -4,6 +4,7 @@ require_once("conexion.php");
 
 $fichatrans= $_POST['trans'];
 $documento = $_POST['docu'];
+$ficha = $_POST['ficha'];
 $materia = $_POST['materia'];
 $dias = $_POST['dias'];
 $ambiente= $_POST['ambi'];
@@ -21,7 +22,7 @@ if ($ficha == $info['no_ficha'] & $materia == $info['id_materia']) {
     echo "La asignacion ya existe en la formacion";
 }else{
 
-    $insertar = "INSERT INTO asignacion_t (ficha_trans,docu,no_ficha,id_materia,id_amb,id_dia,horario_inicio,horario_fin,fecha_inico,fecha_final) values ('$fichatrans',$documento','$ficha','$materia','$ambiente','$dias','$hora_inicio','$hora_fin','$fecha_inicio','$fecha_fin')";
+    $insertar = "INSERT INTO asignacion_t (`ficha_trans`, `docu`, `no_ficha`, `id_materia`, `Id_dia`, `id_amb`, `horario_inicio`, `horario_fin`, `fecha_inico`, `fecha_final`) values ($fichatrans ,$documento, $ficha, $materia, $dias, $ambiente, '$hora_inicio', '$hora_fin','$fecha_inicio', '$fecha_fin')";
     $sql = mysqli_query($bdmysqli,$insertar);
 
     if ($sql) {

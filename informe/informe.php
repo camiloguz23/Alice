e6fe0656fd6340e6cc955ca1a1f28972b34708ac
jeyson camilo <jeysonkmguzman@gmail.com>
2021-07-24 10:53:23 -0500

    session informes estructura

---
+++ informe/informe.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Informe de las asignaciones</title>
+</head>
+<body>
+    <header>
+        <img src="../img/ashleylogo.png" alt="" class="logo">
+        <ul>
+            <li>Instructores Tecnicos</li>
+            <li>Instructores Transversales</li>
+            <li>informe general</li>
+            <li class="documento">Por documento</li>
+        </ul>
+        <div class="buscador">
+            <input type="number" name="" id="" placeholder="Buscador por el documento">
+        </div>
+    </header>
+    <div id="contenido"></div>
+</body>
+</html>

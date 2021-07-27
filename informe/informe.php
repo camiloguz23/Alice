f8a7ad085012c03bd3d8c0d3e8687aa7adcbc98e
SubaDanirojas <danielalejandrobastidasrojas@gmail.com>
2021-07-27 11:18:00 -0500

    informe con divs

--- informe/informe.php
+++ informe/informe.php
@@ -1,3 +1,16 @@
+<?php
+include ('../php/conexion.php');
+
+?>
+<?php
+$titul = "SELECT * FROM usuario WHERE id_tip_usu = 3";
+$titulado = mysqli_query($bdmysqli,$titul);
+
+$trans = "SELECT * FROM usuario WHERE id_tip_usu = 4";
+$transversal = mysqli_query($bdmysqli,$trans);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,16 +24,60 @@
     <header>
         <img src="../img/ashleylogo.png" alt="" class="logo">
         <ul>
-            <li>Instructores Tecnicos</li>
-            <li>Instructores Transversales</li>
-            <li>informe general</li>
+        <li type="button" onclick="mostrarTecnicos();">Instructores Tecnicos</li>
+            <li type="button" onclick="mostrarTransversal();">Instructores Transversales</li>
+            <li>Informe general</li>
             <li class="documento">Por documento</li>
         </ul>
+
+        <script type="text/javascript">
+		function mostrarTecnicos() {
+			document.getElementById('tecnicos').style.display = 'block';
+            document.getElementById('transversal').style.display = 'none';
+		}
+        function mostrarTransversal() {
+			document.getElementById('transversal').style.display = 'block';
+            document.getElementById('tecnicos').style.display = 'none';
+		}
+
+	    </script>
         <div class="buscador">
             <input type="number" name="" id="" placeholder="Buscador por el documento">
         </div>
         <a href="../usuarios/admin/admin.php"><img src="../img/salida.png" alt="salida" class="salida"></a>
     </header>
-    <div id="contenido"></div>
+    <body>
+        <div id="contenido">
+            <div id="tecnicos">
+                    <?php $resultado = mysqli_query($bdmysqli,$titul);
+                    while($row=mysqli_fetch_assoc($resultado)) {?>
+                        <div class="main-container"> 
+                            <p class="table__item"> <b>DOCUMENTO:</b><?php echo $row["docu"];?></p>
+                            <p class="table__item"> <b>NOMBRE:</b><?php echo $row["nombres"];?></p>
+                            <p class="table__item"> <b>APELLIDO:</b><?php echo $row["apellidos"];?></p>
+                            <p class="table__item"> <b>EDAD:</b><?php echo $row["edad"];?></p>
+                            <p class="table__item"> <b>CELULAR:</b><?php echo $row["celular"];?></p>
+                            <p class="table__item"> <b>FIJO:</b><?php echo $row["fijo"];?></p>
+                            <p class="table__item"> <b>DIRECCION:</b><?php echo $row["direccion"];?></p>
+                        </div>
+                            <?php } mysqli_free_result($resultado); ?>
+        
+        </div>
+
+        <div id="transversal">
+                    <?php $resulta = mysqli_query($bdmysqli,$trans);
+                    while($row=mysqli_fetch_assoc($resulta)) {?>
+                        <div class="main-container"> 
+                            <p class="table__item"> <b>DOCUMENTO:</b><?php echo $row["docu"];?></p>
+                            <p class="table__item"> <b>NOMBRE:</b><?php echo $row["nombres"];?></p>
+                            <p class="table__item"> <b>APELLIDO:</b><?php echo $row["apellidos"];?></p>
+                            <p class="table__item"> <b>EDAD:</b><?php echo $row["edad"];?></p>
+                            <p class="table__item"> <b>CELULAR:</b><?php echo $row["celular"];?></p>
+                            <p class="table__item"> <b>FIJO:</b><?php echo $row["fijo"];?></p>
+                            <p class="table__item"> <b>DIRECCION:</b><?php echo $row["direccion"];?></p>
+                        </div>
+                            <?php } mysqli_free_result($resulta); ?>
+        
+        </div>
 </body>
 </html>

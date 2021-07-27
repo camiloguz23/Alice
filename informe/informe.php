9cc7e5917caa8b11b973cb4a7e7526ed43e348fb
jhon <67174204+chiqky@users.noreply.github.com>
2021-07-27 12:03:07 -0500

    hola

--- informe/informe.php
+++ informe/informe.php
@@ -1,25 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Informe de las asignaciones</title>
+	<meta charset="UTF-8">
+	<title>INFORME</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<script src="https://kit.fontawesome.com/18e932af55.js"></script>
+	<link rel="stylesheet" href="CSS/style.css">
+	<script src="JS/jquery-3.4.1.min.js"></script>
+	<script src="JS/buscar.js"></script>
 </head>
 <body>
-    <header>
-        <img src="../img/ashleylogo.png" alt="" class="logo">
+	
+	 <header>
+
+        
+	 
+	 <img src="../img/ashleylogo.png" alt="" class="logo">
         <ul>
-            <li>Instructores Tecnicos</li>
-            <li>Instructores Transversales</li>
-            <li>informe general</li>
+            <li href="informetecnico.php" >Instructores Tecnicos</li>
+            <li href="informetecnico.php">Instructores Transversales</li>
+            <li href="informetecnico.php">informe general</li>
             <li class="documento">Por documento</li>
         </ul>
         <div class="buscador">
-            <input type="number" name="" id="" placeholder="Buscador por el documento">
-        </div>
-    </header>
-    <div id="contenido"></div>
+        <input  type="text" name="termino" id="termino" placeholder="BUSCAR" aria-label="Search">
+        </div>	
+	</header>
+	
+	<div id="contenido">
+    		
+			<div class="todo" id="tabla_resultados">
+				
+			</div>
+	
+
+	</div>
+	
 </body>
 </html>

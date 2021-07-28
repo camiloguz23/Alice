234395b76f495c238aef2abecc7e7cff5528817b
jeyson camilo <jeysonkmguzman@gmail.com>
2021-07-27 23:42:57 -0500

    cambio de informes

--- informe/informe.php
+++ informe/informe.php
@@ -14,10 +14,9 @@ $titulado = mysqli_query($bdmysqli,$titul);
 	<meta charset="UTF-8">
 	<title>INFORME</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<script src="https://kit.fontawesome.com/18e932af55.js"></script>
-	<link rel="stylesheet" href="CSS/style.css">
-    <script src="js/jquery-3.4.1.min.js"></script>
-	<script src="js/buscar.js"></script>
+	
+	<link rel="stylesheet" href="css/style.css">
+    
 	
 </head>
 <body>
@@ -27,51 +26,31 @@ $titulado = mysqli_query($bdmysqli,$titul);
         
 	 
 	 <img src="../img/ashleylogo.png" alt="" class="logo">
-        <ul>
-        <li type="button" onclick="mostrarTecnicos();">Instructores Tecnicos</li>
+        <ul class="documento">
+        <li >Instructores Tecnicos</li>
             <li>Instructores Transversales</li>
             <li>Informe general</li>
-            <li class="documento">Por documento</li>
+            <li >Por documento</li>
         </ul>
+        <div class="buscador">
+        <input  type="text" name="termino" id="ficha" placeholder="numero de ficha" aria-label="Search">
+        </div>
+        <div class="buscador">
+        <input  type="text" name="termino" id="ambiente" placeholder="ambiente" aria-label="Search">
+        </div>
 
         <div class="buscador">
-        <input  type="text" name="termino" id="termino" placeholder="BUSCAR" aria-label="Search">
+        <input  type="text" name="termino" id="termino" placeholder="documento" aria-label="Search">
         </div>	
-
-        <script type="text/javascript">
-		function mostrarTecnicos() {
-			document.getElementById('tecnicos').style.display = 'block';
-            document.getElementById('transversal').style.display = 'none';
-		}
-
-	    </script>
-         
         
         <a href="../usuarios/admin/admin.php"><img src="../img/salida.png" alt="salida" class="salida"></a>
     </header>
     
-        <div id="contenido">
-    		
-			<div class="todo" id="tabla_resultados">
-            </div>
-				
+        <div id="contenido" class="contenido">
+            		
 		</div>
         
-        <div id="tecnicos">
-                    <?php $resultado = mysqli_query($bdmysqli,$titul);
-                    while($row=mysqli_fetch_assoc($resultado)) {?>
-                        <div class="main-container"> 
-                            <p class="table__item"> <b>FICHA:</b><?php echo $row["no_ficha"];?></p>
-                            <p class="table__item"> <b>DOCUMENTO:</b><?php echo $row["docu"];?></p>
-                            <p class="table__item"> <b>NOMBRE:</b><?php echo $row["nombres"];?></p>
-                            <p class="table__item"> <b>APELLIDO:</b><?php echo $row["apellidos"];?></p>
-                            <p class="table__item"> <b>EDAD:</b><?php echo $row["edad"];?></p>
-                            <p class="table__item"> <b>CELULAR:</b><?php echo $row["celular"];?></p>
-                            <p class="table__item"> <b>FIJO:</b><?php echo $row["fijo"];?></p>
-                            <p class="table__item"> <b>DIRECCION:</b><?php echo $row["direccion"];?></p>
-                        </div>
-                            <?php } mysqli_free_result($resultado); ?>
-        </div>
-    
+        
+    <script src="JS/buscar.js"></script>
 </body>
 </html>
